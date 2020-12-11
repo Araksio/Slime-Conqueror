5cfe39294e768bd64d8d150a60ea52a018b75eba
Onchigo <kiruuaizumaa@gmail.com>
2020-12-11 13:39:01 +0100

    Javadoc MyGameMenuController

--- src/main/java/GUI/MyGameMenuController.java
+++ src/main/java/GUI/MyGameMenuController.java
@@ -10,6 +10,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.media.AudioClip;
 
+/**
+ * @author Gaël
+ * MyGameMenuController permet de générer un menu pause en jeu.
+ * Ce menu est composé de :
+ * 
+ * Un boutton pour reprendre la partie
+ * Un boutton options
+ * Un boutton revenant au menu principal
+ * Un boutton pour quitter le jeu.
+ */
 public class MyGameMenuController {
 	
  @FXML
@@ -22,15 +32,18 @@ public class MyGameMenuController {
  private Button exitButton;
 
 
- @FXML
- 
  /**
-  * fonction de FXGL qui initialise le menu pause avec les boutons
-  * 
-  * @author Gael
-  */
+	 * @author Gaël
+	 * On initialise le fonctionnement des bouttons.
+	 */
+ @FXML
  private void initialize()
  {
+	 /*
+	  * Lorsque l'on passe notre souris sur un des bouttons,
+	  * On joue un petit son et le boutton devient orangé
+	  * Puis lorsque l'on quitte le boutton, il reprends sa couleur originelle.
+	  */
 	 resumeButton.setOnMouseEntered(e -> { 
 	           resumeButton.setStyle("-fx-background-color: #914206; ");
 	           buttonSFX();
@@ -58,54 +71,52 @@ public class MyGameMenuController {
 
  }
  
-
  /**
-  * fonction pour le son des boutons 
-  * @author Gael
+  * @author Gaël
+  * Cette méthode permet de jouer un petit son lorsqu'elle est appelé.
   */
 	public void buttonSFX()
 	{
-		AudioClip a1 = new AudioClip(getClass().getResource("/SFX/buttonSound.mp3").toString());
-		a1.play();
+		AudioClip a1 = new AudioClip(getClass().getResource("/SFX/buttonSound.mp3").toString()); // On cherche le son en .mp3 dans SFX
+		a1.play(); // Et on le joue
 	}
 
 
+	/**
+	 * @author Gaël
+	 * Cette méthode permet de reprendre le jeu lorsqu'elle est appelé
+	 */
  @FXML
- /**
-  * fonction de FXGL qui relance le jeu
-  * 
-  * @author Gael
-  */
   private void resumeGame()
   {
-	 FXGL.getGameController().gotoPlay();
+	 
+	 FXGL.getGameController().gotoPlay(); // Revient au jeu.
   }
- @FXML
  
  /**
-  * fonction de FXGL qui renvoie au menu principal  
-  *  
-  * @author Gael
-  */
+	 * @author Gaël
+	 * Cette méthode permet de revenir au menu principal lorsqu'elle est appelé
+	 */
+ @FXML
  private void goToMainMenu()
  {
-      FXGL.getGameScene().removeChild(GameApp.layout);
-		if( InGameController.scheduledExecutorService != null)
+      FXGL.getGameScene().removeChild(GameApp.layout); // On enlève le layout du jeu
+		if( InGameController.scheduledExecutorService != null) // On regarde si il y a un rafraichissement en cours
 		{
-			 InGameController.scheduledExecutorService.shutdown();
+			 InGameController.scheduledExecutorService.shutdown(); // On l'arrête
 		}
-	 FXGL.getGameController().gotoMainMenu();
+	 FXGL.getGameController().gotoMainMenu(); // On revient au menu principal
 	 
  }
- @FXML
+ 
  /**
-  * fonction de FXGL qui ferme le jeu
-  * 
-  * @author Gael
-  */
+	 * @author Gaël
+	 * Cette méthode permet de quitter le jeu lorsqu'elle est appelé
+	 */
+ @FXML
  private void exitGame()
  {
-	 InGameController.scheduledExecutorService.shutdown();
-	 FXGL.getGameController().exit();
+	 InGameController.scheduledExecutorService.shutdown(); // On arrête le rafraîchissement auto du jeu
+	 FXGL.getGameController().exit(); // Et on ferme le jeu complètement
  }
 }
